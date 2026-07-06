d28a61e5c5bdfc9e78f1213477beb4a2abc67d5a
mreisner2000 <mreisner2000@gmail.com>
2026-07-06 09:35:58 +0200

    Version 3.0 - Stable foundation before refactoring

--- config.js
+++ config.js
@@ -8,5 +8,13 @@ const CONFIG = {
     startDate: new Date("2026-07-01T00:00:00"),
     targetDate: new Date("2026-07-31T14:00:00"),
 
-    journalRefreshTime: 15 * 60 * 1000
+    journalRefreshTime: 15 * 60 * 1000,
+
+    probabilities: {
+        golden: 0.08,
+        easterEgg: 0.04,
+        personal: 0.12,
+        humor: 0.25,
+        festival: 0.20
+    }
 };
