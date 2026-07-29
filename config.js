181c2d7dc183621ad4cb7d93187d3072310b6978
mreisner2000 <mreisner2000@gmail.com>
2026-07-29 23:43:07 +0200

    Final Test set target

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     festivalName: "Acoustic Campfire Festival",
 
     startDate: new Date("2026-07-01T00:00:00"),
-    targetDate: new Date("2026-07-31T14:00:00"),
+    targetDate: new Date("2026-07-29T14:00:00"),
 
     journalRefreshTime: 15 * 60 * 1000,
 
